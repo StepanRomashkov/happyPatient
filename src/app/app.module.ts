ef73736a438a7d587f5b6b92cfbcfb954dd75247
Stepan Romashkov <akumajo27@gmail.com>
2017-06-24 16:14:13 -0400

    added header and appointment component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SetAppointmentComponent } from './set-appointment/set-appointment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetAppointmentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/set-appointment/set-appointment.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-set-appointment',
+  templateUrl: './set-appointment.component.html',
+  styleUrls: ['./set-appointment.component.css']
+})
+export class SetAppointmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
