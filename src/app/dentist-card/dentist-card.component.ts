1aed52b3e3095ee1b317c98c12128d186f431611
nxfan <nxfan@umich.edu>
2017-06-24 21:11:05 -0400

    dentist-card

---
+++ src/app/dentist-card/dentist-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dentist-card',
+  templateUrl: './dentist-card.component.html',
+  styleUrls: ['./dentist-card.component.css']
+})
+export class DentistCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
